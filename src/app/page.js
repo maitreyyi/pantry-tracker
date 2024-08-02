38c4ad400f28f94dc4fd5f2d4466e7b457b86b26
maitreyyi <matreyisinha@gmail.com>
2024-08-01 20:36:29 -0700

    Add initial design of nav bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,8 @@
 'use client'
-import { Button, Box, Stack, Modal, TextField, Typography } from '@mui/material';
+import { AppBar, Toolbar, InputBase, IconButton, Button, Box, Stack, Modal, TextField, Typography} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import MenuIcon from '@mui/icons-material/Menu';
+import {styled, alpha} from '@mui/material/styles';
 import { firestore } from '../../firebase';
 import {useState,useEffect} from 'react';
 import {collection, getDocs, query, getDoc, deleteDoc, doc, setDoc} from 'firebase/firestore';
@@ -25,6 +28,49 @@ export default function Home() {
 
     setInventory(inventoryList)
   }
+
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+  
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    width: '100%',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }));
+  
   const removeItem = async (item) => {
     const docRef = doc(collection(firestore, 'pantry'), item)
     const docSnap = await getDoc(docRef)
@@ -64,66 +110,103 @@ export default function Home() {
   const handleClose = () => setOpen(false)
 
   return (
-    <Box width = "100vw" height="100vh" display="flex" justifyContent="center" alignItems="center" flexDirection="column" gap={2}>
-      <Modal open={open} onClose={handleClose}>
-        <Box 
-          position="absolute" 
-          top="50%" 
-          left="50%" 
-          width={400} 
-          bgcolor="white" 
-          border="2px solid #000" 
-          boxShadow={24} 
-          p={4} 
-          display="flex" 
-          flexDirection="column" 
-          gap={3} 
-          sx={{ 
-            transform: 'translate(-50%,-50%)',
-          }}>
-          <Typography variant = "h6">Add Item</Typography>
-          <Stack width="100%" direction="row" spacing={2}>
-            <TextField 
-              variant='outlined' 
-              fullWidth 
-              value={itemName} 
-              onChange={(e) =>{
-                setItemName(e.target.value)
-              }}>
-            </TextField>
-            <Button variant="outlined" onClick={() => {
-                addItem(itemName)
-                setItemName('')
-                handleClose()
-              }}>
-            +
-            </Button>
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            MUI
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+      <Box width = "100vw" height="100vh" display="flex" justifyContent="center" alignItems="center" flexDirection="column" gap={2}>
+        <Modal open={open} onClose={handleClose}>
+          <Box 
+            position="absolute" 
+            top="50%" 
+            left="50%" 
+            width={400} 
+            bgcolor="white" 
+            border="2px solid #000" 
+            boxShadow={24} 
+            p={4} 
+            display="flex" 
+            flexDirection="column" 
+            gap={3} 
+            sx={{ 
+              transform: 'translate(-50%,-50%)',
+            }}>
+            <Typography variant = "h6">Add Item</Typography>
+            <Stack width="100%" direction="row" spacing={2}>
+              <TextField 
+                variant='outlined' 
+                fullWidth 
+                value={itemName} 
+                onChange={(e) =>{
+                  setItemName(e.target.value)
+                }}>
+              </TextField>
+              <Button variant="outlined" onClick={() => {
+                  addItem(itemName)
+                  setItemName('')
+                  handleClose()
+                }}>
+              +
+              </Button>
+            </Stack>
+          </Box>
+        </Modal>
+
+        <Box border='3px solid #333'>
+          <Box width = "800px" height="100px" bgcolor="#ADD8E6" alignItems="center" display="flex" justifyContent={"center"}>
+            <Typography variant='h2' color='#333'>
+              Inventory Items
+            </Typography>
+          </Box>
+
+          <Stack width="800px" height="300px" spacing={2} overflow="auto">
+            {
+              inventory.map(({name, quantity})=>(
+                <Box key={name} width="100%" minHeight="150px" display="flex" justifyContent="space-between" bgColor='#f0f0f0' padding={5}>
+                  <Typography variant='h5' color='#333' textAlign={'center'}>{name.charAt(0).toUpperCase() + name.slice(1)}</Typography>
+                  <Typography variant='h5' color='#333' textAlign={'center'}>{quantity}</Typography>
+                  <Button variant="contained" onClick={()=>(
+                    removeItem(name)
+                  )}>-</Button>
+                  <Button variant="contained" onClick={()=>(
+                    addItem(name)
+                  )}>+</Button>
+                </Box>
+            ))}
           </Stack>
         </Box>
-      </Modal>
-      <Button variant="contained" onClick={()=>(
-        handleOpen()
-      )}>Add New Item</Button>
 
-      <Box border='3px solid #333'>
-        <Box width = "800px" height="100px" bgcolor="#ADD8E6" alignItems="center" display="flex" justifyContent={"center"}>
-          <Typography variant='h2' color='#333'>
-            Inventory Items
-          </Typography>
-        </Box>
+        <Button variant="contained" onClick={()=>(
+          handleOpen()
+        )}>Add New Item</Button>
 
-        <Stack width="800px" height="300px" spacing={2} overflow="auto">
-          {
-            inventory.map(({name, quantity})=>(
-              <Box key={name} width="100%" minHeight="150px" display="flex" justifyContent="space-between" bgColor='#f0f0f0' padding={5}>
-                <Typography variant='h5' color='#333' textAlign={'center'}>{name.charAt(0).toUpperCase() + name.slice(1)}</Typography>
-                <Typography variant='h5' color='#333' textAlign={'center'}>{quantity}</Typography>
-                <Button variant="contained" onClick={()=>(
-                  removeItem(name)
-                )}>-</Button>
-              </Box>
-          ))}
-        </Stack>
       </Box>
     </Box>
   )
