f12d1bb3b4c154680463623f74a0c545c106aa71
maitreyyi <matreyisinha@gmail.com>
2024-08-02 16:22:50 -0700

    Working sidebar added

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
 'use client'
-import { AppBar, Toolbar, InputBase, IconButton, Button, Box, Stack, Modal, TextField, Typography} from '@mui/material';
+import { Drawer, AppBar, Toolbar, InputBase, IconButton, Button, Box, Stack, Modal, TextField, List, ListItem, ListItemButton,ListItemText, Divider, Typography} from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import MenuIcon from '@mui/icons-material/Menu';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import MailIcon from '@mui/icons-material/Mail';
 import {styled, alpha} from '@mui/material/styles';
 import { firestore } from '../../firebase';
 import {useState,useEffect} from 'react';
@@ -11,6 +14,7 @@ import {collection, getDocs, query, getDoc, deleteDoc, doc, setDoc} from 'fireba
 export default function Home() {
   const [inventory, setInventory] = useState([])
   const [open, setOpen] = useState(false)
+  const [openMenu, setMenuOpen] = useState(false)
   const [itemName, setItemName] = useState('')
 
   const searchInventory = async(item) => {
@@ -27,6 +31,13 @@ export default function Home() {
     setInventory(inventoryList)
   }
 
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  const handleToggleMenu = (bool) => () => {
+    setMenuOpen(bool);
+  };
+
   const updateInventory = async () => {
     const snapshot = query(collection(firestore, 'pantry'))
     const docs = await getDocs(snapshot)
@@ -42,6 +53,36 @@ export default function Home() {
 
     setInventory(inventoryList)
   }
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={handleToggleMenu(false)}>
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   const Search = styled('div')(({ theme }) => ({
     position: 'relative',
@@ -119,30 +160,16 @@ export default function Home() {
   useEffect(() => {
     updateInventory()
   }, [])
-
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
  
   return (
     <Box>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="white"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
+          <IconButton size="large" edge="start" color="white" aria-label="open drawer" sx={{ mr: 2 }}>
+            <MenuIcon onClick = {handleToggleMenu(true)}/>
+            <Drawer  open={openMenu} onClose={handleToggleMenu(false)}>{DrawerList}</Drawer>
           </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-            onClick={updateInventory}
-          >
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }} onClick={updateInventory}>
             Pantry Tracker
           </Typography>
           <Search>
@@ -163,21 +190,7 @@ export default function Home() {
       </AppBar>
       <Box width = "100vw" height="100vh" display="flex" justifyContent="center" alignItems="center" flexDirection="column" gap={2}>
         <Modal open={open} onClose={handleClose}>
-          <Box 
-            position="absolute" 
-            top="50%" 
-            left="50%" 
-            width={400} 
-            bgcolor="white" 
-            border="2px solid #000" 
-            boxShadow={24} 
-            p={4} 
-            display="flex" 
-            flexDirection="column" 
-            gap={3} 
-            sx={{ 
-              transform: 'translate(-50%,-50%)',
-            }}>
+          <Box position="absolute" top="50%" left="50%" width={400} bgcolor="white" border="2px solid #000" boxShadow={24} p={4} display="flex" flexDirection="column" gap={3} sx={{ transform: 'translate(-50%,-50%)',}}>
             <Typography variant = "h6">Add Item</Typography>
             <Stack width="100%" direction="row" spacing={2}>
               <TextField 
@@ -201,7 +214,7 @@ export default function Home() {
 
         <Box border='3px solid #333'>
           <Box width = "800px" height="100px" bgcolor="#ADD8E6" alignItems="center" display="flex" justifyContent={"center"}>
-            <Typography variant='h2' color='#333'>
+            <Typography variant='h3' color='#333'>
               Inventory Items
             </Typography>
           </Box>
@@ -223,9 +236,7 @@ export default function Home() {
           </Stack>
         </Box>
 
-        <Button variant="contained" onClick={()=>(
-          handleOpen()
-        )}>Add New Item</Button>
+        <Button variant="contained" onClick={()=>(handleOpen())}>Add New Item</Button>
 
       </Box>
     </Box>
